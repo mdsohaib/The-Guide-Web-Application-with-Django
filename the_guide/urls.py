f9d1919bc1cde9097b0a062b08164a0eb26280f1
Md Sohaib <mdsohaibuddin@gmail.com>
2021-08-04 20:38:25 +0530

    Update urls.py

--- the_guide/urls.py
+++ the_guide/urls.py
@@ -20,6 +20,8 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='index'),
-    path('guide', views.guide, name='guide')
+    path('', include("Home.urls")),
+
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
